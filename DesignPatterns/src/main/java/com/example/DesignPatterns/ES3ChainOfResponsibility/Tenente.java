9594644db94f89ca57c6c9aa2da0df0339477264
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 17:00:29 +0200

    Update Tenente.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Tenente.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Tenente.java
@@ -1,5 +1,20 @@
 package com.example.DesignPatterns.ES3ChainOfResponsibility;
 
-public class Tenente {
+public class Tenente extends Ufficiale {
+
+	public Tenente() {
+		super(3000);
+	}
+
+	@Override
+	public void controlloImporto(int importo) {
+		if (importo <= stipendio) {
+			System.out.println("Il Tenente percepisce lo stipendio considerato");
+		} else if (nextUfficiale != null) {
+			nextUfficiale.controlloImporto(importo);
+		} else {
+			System.out.println("Nessun Ufficiale percepisce lo stipendio considerato");
+		}
+	}
 
 }
