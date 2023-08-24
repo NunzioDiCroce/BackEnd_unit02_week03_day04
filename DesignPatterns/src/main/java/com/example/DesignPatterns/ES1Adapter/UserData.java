4c1215040c62fcfbf4e4260bbd2185cf02c8de26
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 15:01:20 +0200

    Create DataSource.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES1Adapter/UserData.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES1Adapter/UserData.java
@@ -1,5 +1,17 @@
 package com.example.DesignPatterns.ES1Adapter;
 
+import javax.sql.DataSource;
+
 public class UserData {
+
+	private String NomeCompleto;
+	private int eta;
+
+	public void getData(DataSource ds) {
+
+		nomeCompleto = ds.getNomeCompleto();
+		eta = ds.getEta();
+
+	}
 
 }
