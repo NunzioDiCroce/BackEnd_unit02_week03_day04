13ee858ce95473dc1faa4dc4accef68b251ede51
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 15:32:57 +0200

    Update MyAdapter.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES1Adapter/MyAdapter.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES1Adapter/MyAdapter.java
@@ -2,6 +2,12 @@ package com.example.DesignPatterns.ES1Adapter;
 
 public class MyAdapter implements DataSource {
 
+	private Info info;
+
+	public MyAdapter(Info info) {
+		this.info = info;
+	}
+
 	@Override
 	public String getNomeCompleto() {
 		// TODO Auto-generated method stub
