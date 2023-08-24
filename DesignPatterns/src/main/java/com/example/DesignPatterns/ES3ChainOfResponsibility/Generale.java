d05cda4da7c5a7524dca4f8261fbc78e73374e3c
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 17:07:26 +0200

    Update Generale.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Generale.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Generale.java
@@ -1,5 +1,20 @@
 package com.example.DesignPatterns.ES3ChainOfResponsibility;
 
-public class Generale {
+public class Generale extends Ufficiale {
+
+	public Generale() {
+		super(5000);
+	}
+
+	@Override
+	public void controlloImporto(int importo) {
+		if (importo <= stipendio) {
+			System.out.println("Il Generale percepisce lo stipendio considerato");
+		} else if (nextUfficiale != null) {
+			nextUfficiale.controlloImporto(importo);
+		} else {
+			System.out.println("Nessun Ufficiale percepisce lo stipendio considerato");
+		}
+	}
 
 }
