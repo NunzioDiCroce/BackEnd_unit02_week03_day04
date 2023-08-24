166a6e3e022a6da88e12f8078b7f6a09da36328c
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 17:06:36 +0200

    Update Colonnello.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Colonnello.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Colonnello.java
@@ -1,5 +1,20 @@
 package com.example.DesignPatterns.ES3ChainOfResponsibility;
 
-public class Colonnello {
+public class Colonnello extends Ufficiale {
+
+	public Colonnello() {
+		super(4000);
+	}
+
+	@Override
+	public void controlloImporto(int importo) {
+		if (importo <= stipendio) {
+			System.out.println("Il Colonnello percepisce lo stipendio considerato");
+		} else if (nextUfficiale != null) {
+			nextUfficiale.controlloImporto(importo);
+		} else {
+			System.out.println("Nessun Ufficiale percepisce lo stipendio considerato");
+		}
+	}
 
 }
