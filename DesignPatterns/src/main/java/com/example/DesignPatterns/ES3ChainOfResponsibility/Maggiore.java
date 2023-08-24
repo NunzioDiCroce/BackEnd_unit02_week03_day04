3a928a53477e058e06925ba07bfd0307fb1d2d2a
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 16:59:55 +0200

    Update Maggiore.java

--- DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Maggiore.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Maggiore.java
@@ -1,5 +1,20 @@
 package com.example.DesignPatterns.ES3ChainOfResponsibility;
 
-public class Maggiore {
+public class Maggiore extends Ufficiale {
+
+	public Maggiore() {
+		super(2000);
+	}
+
+	@Override
+	public void controlloImporto(int importo) {
+		if (importo <= stipendio) {
+			System.out.println("Il Maggiore percepisce lo stipendio considerato");
+		} else if (nextUfficiale != null) {
+			nextUfficiale.controlloImporto(importo);
+		} else {
+			System.out.println("Nessun Ufficiale percepisce lo stipendio considerato");
+		}
+	}
 
 }
