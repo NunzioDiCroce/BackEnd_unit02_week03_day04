f65f80e4a7d384bf3f416ce707d6b913c71167c9
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 16:56:47 +0200

    Create Client.java

---
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES3ChainOfResponsibility/Client.java
@@ -0,0 +1,22 @@
+package com.example.DesignPatterns.ES3ChainOfResponsibility;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Capitano capitano = new Capitano();
+		Maggiore maggiore = new Maggiore();
+		Tenente tenente = new Tenente();
+		Colonnello colonnello = new Colonnello();
+		Generale generale = new Generale();
+
+		capitano.setNextUfficiale(maggiore);
+		maggiore.setNextUfficiale(tenente);
+		tenente.setNextUfficiale(colonnello);
+		colonnello.setNextUfficiale(generale);
+
+		int importoVerifica = 3500;
+
+		capitano.controlloImporto(importoVerifica);
+	}
+
+}
