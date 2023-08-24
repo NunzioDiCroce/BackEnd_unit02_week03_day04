2fe584fe44c83108dba79e447ecc2a5920d73086
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-24 15:12:42 +0200

    Create Info.java

---
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/ES1Adapter/Info.java
@@ -0,0 +1,35 @@
+package com.example.DesignPatterns.ES1Adapter;
+
+import java.util.Date;
+
+public class Info {
+
+	private String nome;
+	private String cognome;
+	private Date dataDiNascita;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataDiNascita(Date dataDiNascita) {
+		return this.dataDiNascita = dataDiNascita;
+	}
+
+	public void DataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+}
